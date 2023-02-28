221b5400da80a708d2ec7d0468c7081e3d66dbc2
emilalmander <emilalmander@hotmail.se>
2023-02-28 19:33:34 +0000

    add game functions

---
+++ js/script.js
@@ -0,0 +1,31 @@
+function RunGame() {
+
+}
+
+function CheckAnswer() {
+
+}
+
+function CalculateCorrectAnswer (){
+
+}
+
+function IncrementScore(){
+
+}
+
+function IncrementWrongAnswer(){
+
+}
+
+function DisplayAdittionQuestion(){
+
+}
+
+function DisplaySubtractQuestion(){
+
+}
+
+function DisplayMultipliQuestion(){
+    
+}
