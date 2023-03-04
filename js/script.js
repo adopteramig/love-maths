d7f83075e6d5491f33f9c5309d3264785bf17f0e
emilalmander <emilalmander@hotmail.se>
2023-03-04 14:36:01 +0000

    add even listeners for button clickcks

--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
+document.addEventListener("DOMContentLoaded", function(){
+    let buttons = document.getElementsByTagName("button");
+    
+          for (let button of buttons){
+             button.addEventListener("click", function(){
+                 if (this,this.getAttribute("data-type") === "submit")  { 
+                 alert("you clicked submit!");
+            } else {
+                let gameType = this.getAttribute("data-type")
+                alert(`You clicked ${gameType}`);
+        }
+
+            
+        })
+    }
+})
+
+
 function runGame() {
 
 }
